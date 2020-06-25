541c080b509a0b1bd69ce2f27d844d9992f8654a
Marcelo Campanelli <marcelo.campanelli@yahoo.com>
2020-06-25 17:44:40 -0300

    1modulo de componente terminado

---
+++ vue-cli/projeto-simples/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="app">
+      {{ titulo }}
+      <button @click="titulo += '#'"
+> Add</button>
+    </div>
+</template>
+
+<script >
+export default{
+  data: function(){
+    return{
+      titulo: 'Teste Data Usando VueJS !!'
+    }
+  }
+}
+</script>
+
+
+<style >
+  #app{
+    background-color:chocolate ;
+    color: white
+  }
+
+
+</style>
